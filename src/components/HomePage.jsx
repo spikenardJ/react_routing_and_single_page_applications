0cae3cd06d8009ab07527a30a78c483a1f291f64
spikenardJ <jennifer_annette@hotmail.com>
2025-01-12 17:13:52 -0600

    First commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,57 +1,62 @@
 import React from "react";
 import { Button, Card, CardGroup, Image } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import storeImage from "../images/store.jpg";
+import itImage from "../images/it.jpg";
+import kitchenImage from "../images/kitchen.jpg";
+import toyImage from "../images/toy.jpg"
 import "../Style.css"
 
 const HomePage = () => {
     return (
-      <div style={{ backgroundColor: '#f8f9fa', padding: '20px', border: '2px solid #ddd', borderRadius: '8px' }}>
-        <h1 style={{ color: '#343a40' }}>Welcome to Our Store</h1>
+      <div style={{ backgroundColor: "#f8f9fa", padding: "20px", border: "2px solid #ddd", borderRadius: "8px" }}>
+        <h1 className="mb-5">Welcome to the Corner Store</h1>
+        <h3>Online Shop!</h3>
         
-        {/* Responsive Image */}
         <Image 
-          src="/assets/welcome-image.jpg" 
+          src={storeImage} 
           alt="Welcome" 
           fluid 
-          className="mb-4" 
+          className="mb-4 rounded"
+          border="2px solid #ddd"
         />
   
         <div className="mt-4">
           <CardGroup>
             <Card className="card" style={{ width: '18rem', margin: '10px'}}>
-                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Img variant="top" src={itImage} />
                 <Card.Body>
-                <Card.Title>Featured Product</Card.Title>
+                <Card.Title>IT Products</Card.Title>
                 <Card.Text>
-                    Check out our latest product collection!
+                    Check out our IT products!
                 </Card.Text>
-                <Button variant="secondary">View Product</Button>
+                <Button href="/products-page" variant="secondary">IT Products</Button>
                 </Card.Body>
             </Card>
             <Card className="card" style={{ width: '18rem', margin: '10px' }}>
-                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Img variant="top" src={kitchenImage} />
                 <Card.Body>
-                <Card.Title>Featured Product</Card.Title>
+                <Card.Title>Kitchen Products</Card.Title>
                 <Card.Text>
-                    Check out our latest product collection!
+                    Check out our kitchen products!
                 </Card.Text>
-                <Button variant="secondary">View Product</Button>
+                <Button href="/products-page" variant="secondary">Kitchen Products</Button>
                 </Card.Body>
             </Card>
             <Card className="card" style={{ width: '18rem', margin: '10px' }}>
-                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Img variant="top" src={toyImage} />
                 <Card.Body>
-                <Card.Title>Featured Product</Card.Title>
+                <Card.Title>Techy Toys</Card.Title>
                 <Card.Text>
-                    Check out our latest product collection!
+                    Check out our techy toys!
                 </Card.Text>
-                <Button variant="secondary">View Product</Button>
+                <Button href="/products-page" variant="secondary">Techy Toys</Button>
                 </Card.Body>
             </Card>
           </CardGroup>
         </div>
         <div className="d-grid gap-2">
-            <Button variant="primary" size="lg" className="btn shadow-lg mb-4 mt-5">Shop Now</Button>
+            <Button href="/products-page" variant="primary" size="lg" className="btn shadow-lg mb-4 mt-5">Shop Now</Button>
         </div>
       </div>
     );
--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,12 +1,142 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Row, Col, Container, Image, Button } from "react-bootstrap";
+import laptopImage from "../images/laptop.png";
+import iphoneImage from "../images/iphone.png";
+import ipadImage from "../images/ipad.png";
+import potsImage from "../images/pots.png";
+import utensilsImage from "../images/utensils.png";
+import bowlsImage from "../images/bowls.png";
+import hoverImage from "../images/hover.png";
+import robotsImage from "../images/robots.png";
+import droneImage from "../images/drone.png";
 
 function ProductsPage() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
     return (
+
         <div>
-            <p>Sorry, this page is under construction.</p>
-            <p>You can always go back to the <Link to="/">HomePage</Link>.</p>
+        
+        <Container style={{ backgroundColor: "#f8f9fa", padding: "20px", border: "2px solid #ddd", borderRadius: "8px" }}>
+            <Row>
+                <h3 className="it-products">IT Products</h3>
+                <Col xs={8} md={4}>
+                    <Image 
+                        src={laptopImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded" 
+                        width="100%"
+                        border="2px solid #ddd"
+                    />
+                    <p><small>Macbook Laptop: $1500.00</small></p>
+                </Col>
+                <Col xs={8} md={4}>
+                    <Image 
+                        src={iphoneImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded" 
+                        width="100%"
+                        border="2px solid #ddd"
+                    />
+                    <p><small>iPhone: $800.00</small></p>
+                </Col>
+                <Col xs={8} md={4}>
+                    <Image 
+                        src={ipadImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded"
+                        width="100%"
+                        border="2px solid #ddd" 
+                    />
+                    <p><small>iPad: $400.00</small></p>
+                </Col>
+            </Row>
+        </Container>
+        
+        <br /><br /><br />
+        <Container style={{ backgroundColor: "#f8f9fa", padding: "20px", border: "2px solid #ddd", borderRadius: "8px" }}>
+            <Row>
+            <h3 className="kitchen-products">Kitchen Products</h3>
+                <Col xs={8} md={4}>
+                <Image 
+                        src={potsImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded"
+                        width="100%"
+                        border="2px solid #ddd" 
+                    />
+                    <p><small>Copper Pot Set: $1200.00</small></p>
+                </Col>
+                <Col xs={8} md={4}>
+                <Image 
+                        src={utensilsImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded"
+                        width="100%"
+                        border="2px solid #ddd" 
+                    />
+                    <p><small>Teak Utensil Set: $40.00</small></p>
+                </Col>
+                <Col xs={8} md={4}>
+                <Image 
+                        src={bowlsImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded"
+                        width="100%"
+                        border="2px solid #ddd" 
+                    />
+                    <p><small>Bowl Set (4): $30.00</small></p>
+                </Col>
+            </Row>
+        </Container>
+        <br /><br /><br />
+        <Container style={{ backgroundColor: "#f8f9fa", padding: "20px", border: "2px solid #ddd", borderRadius: "8px" }}>
+            <Row>
+            <h3 className="techy-toys">Techy Toys</h3>
+                <Col xs={8} md={4}>
+                <Image 
+                        src={hoverImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded" 
+                        width="100%"
+                        border="2px solid #ddd"
+                    />
+                    <p><small>iPhone: $800.00</small></p>
+                </Col>
+                <Col xs={8} md={4}>
+                <Image 
+                        src={robotsImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded" 
+                        width="100%"
+                        border="2px solid #ddd"
+                    />
+                    <p><small>iPhone: $800.00</small></p>
+                </Col>
+                <Col xs={8} md={4}>
+                <Image 
+                        src={droneImage} 
+                        alt="Welcome" 
+                        className="mb-4 rounded" 
+                        width="100%"
+                        border="2px solid #ddd"
+                    />
+                    <p><small>iPhone: $800.00</small></p>
+                </Col>
+            </Row>
+        </Container>
+        <br />
+            <div className="d-grid gap-2">
+                <Button href="#"variant="secondary" size="lg" className="btn shadow-lg mb-4 mt-5">Shop Now</Button>
+            </div>
         </div>
-    )
+    );
+
 }
+
 
 export default ProductsPage;
